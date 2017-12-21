cd53bb5190c0f9d98af2564ae3e03ae009047f68
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2017-12-21 20:08:19 +0000

    update

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -228,6 +228,7 @@ password=healthchecker
 EOF
 	touch /mysql-init-complete
 	chown -R mysql:mysql "$DATADIR"
+        mysql -u root -proot fda < /tmp/fda.sql
 	echo "[Entrypoint] Starting MySQL 5.7.20-1.1.2"
 
 fi
