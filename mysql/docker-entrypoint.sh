7909d40904033ae7780f94f0e9d30e1e3705a58c
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2017-12-22 14:28:21 +0000

    update

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+#!/bin/bash 
+set -x
 # Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
