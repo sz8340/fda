2150c16f68c02bb74817c1495a80f5e94abd6524
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2017-12-30 01:07:01 +0000

    update

--- src/ContainerCosts.java
+++ src/ContainerCosts.java
@@ -1,4 +1,4 @@
-// File: Inventory2 .java
+// File: ContainerCosts .java
 /* A servlet to display the contents of the MySQL FDA database */
 
 
@@ -10,7 +10,7 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class Inventory2  extends HttpServlet 
+public class ContainerCosts  extends HttpServlet 
 {
     public String getServletInfo()
     {
