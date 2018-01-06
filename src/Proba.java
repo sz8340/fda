0914c725a4b4081aa309741b3204cbc56e2d4d42
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2018-01-06 05:39:12 +0000

    update

--- src/Proba.java
+++
@@ -1,9 +0,0 @@
-public class Proba {
-  public static void main(String[] args) {
- 
-    System.out.println("Linux computer name throguh env:\"" + System.getenv("HOSTNAME") + "\"");
-    //System.out.println("Linux computer name through exec:\"" + execReadToString("hostname") + "\"");
-    //System.out.println("Linux computer name through /etc/hostname:\"" + execReadToString("cat /etc/hostname") + "\"");
-  }
-
-}
