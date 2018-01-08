59729620fbad93c41770f264433b4ace5ae40192
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2018-01-08 15:15:54 +0000

    update

--- src/Title.java
+++ src/Title.java
@@ -20,33 +20,6 @@ public class Title extends HttpServlet
     // Use http GET
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        response.setContentType("text/html");    // Response mime type
-
-        // Output stream to STDOUT
-        PrintWriter out = response.getWriter();
-
-        out.println("<HTML><HEAD><TITLE>Title</TITLE></HEAD>");
-        out.println("<BODY align='center'><H1>FDA Docker Management Tool</H1>");
-
-        Server p = new Server();
-        out.println("hostname: " + p.getName());
-
-    }
-}
-
-
-import javax.servlet.http.*;
-
-public class Applications extends HttpServlet 
-{
-    public String getServletInfo()
-    {
-       return "Servlet connects to MySQL database and displays result of a SELECT";
-    }
-
-    // Use http GET
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
         String loginUser = "Dude1";
         String loginPasswd = "SuperSecret7@";
         String loginUrl = "jdbc:mysql://mysql1:3306/fda";
@@ -56,8 +29,8 @@ public class Applications extends HttpServlet
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
 
-        out.println("<HTML><HEAD><TITLE>Inventory</TITLE></HEAD>");
-        out.println("<BODY align='center'><H1>FDA Docker Management Tool</H1>");
+        out.println("<HTML><HEAD><TITLE>Title</TITLE></HEAD>");
+        out.println("<BODY align='center' bgcolor='lightblue'><H1>FDA Docker Management Tool</H1>");
 
         Server p = new Server();
         out.println("hostname: " + p.getName());
@@ -75,11 +48,12 @@ public class Applications extends HttpServlet
               // Declare our statement
               Statement statement = dbcon.createStatement();
 
-	      String query = "select from fda.applications where application_name='Toris'";
+	      String query = "select * from fda.applications";
 
               PreparedStatement preparedStmt = dbcon.prepareStatement(query);
               preparedStmt.execute();
 
+              out.println("Recorded added!");
               statement.close();
               dbcon.close();
 
