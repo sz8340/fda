6f8602708f8bbb5cc188d47428c73308b2eda07f
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2018-01-08 16:57:19 +0000

    update

--- src/Applications.java
+++ src/Applications.java
@@ -36,8 +36,6 @@ public class Applications extends HttpServlet
 
 	String app=request.getParameter("application");
 	String id=request.getParameter("id");
-        out.println("Application: " + app);
-        out.println("ID: " + id);
 
         // Load the mm.MySQL driver
         try
@@ -54,8 +52,8 @@ public class Applications extends HttpServlet
               PreparedStatement preparedStmt = dbcon.prepareStatement(query);
               preparedStmt.execute();
 
-              out.println("Application: " + app);
-              out.println("ID: " + id);
+              out.println("Application: " + app + "<br>);
+              out.println("ID: " + id + "<br>);
               out.println("Recorded added from hostname: " + p.getName());
               statement.close();
               dbcon.close();
