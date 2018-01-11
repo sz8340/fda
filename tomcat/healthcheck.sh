7920c36d8af3624607156b705fae1276700d3b1e
root <root@ip-172-31-3-58.us-east-2.compute.internal>
2018-01-11 19:19:44 +0000

    update

--- tomcat/healthcheck.sh
+++ tomcat/healthcheck.sh
@@ -18,7 +18,7 @@
 # main server process is started
 if [ -f /tomcat-init-complete ]; # The entrypoint script touches this file
 then # Ping server to see if it is ready
-  curl -f http://tomcat1:8080/ || exit 1
+  curl -f http://localhost:8080/ || exit 1
 else # Initialization still in progress
   exit 1
 fi
